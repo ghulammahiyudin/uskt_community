672cff12f5d5df36f27c8e17daf357cb2be21426
ghula <ghula@GHULAMMAHIYUDIN>
2024-05-28 10:50:32 +0500

    Add project files.

---
+++ USKT_Community/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace USKT_Community.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
